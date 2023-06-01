185888a2e0b0755528cb1405f026b55e97dc06e1
giordani-francelino <francelino.giordani@gmail.com>
2023-06-01 17:23:01 -0300

    inicializcao

--- src/main/java/com/mycompany/contratoTeste/ContratoTeste.java
+++ src/main/java/com/mycompany/contratoTeste/ContratoTeste.java
@@ -1,7 +1,6 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package com.mycompany.contratoTeste;
 
 import com.mycompany.contratoTeste.entidade.Cliente;
@@ -18,16 +17,74 @@ import java.time.format.DateTimeFormatter;
 public class ContratoTeste {
 
     public static void main(String[] args) {
-        new ClienteDao().deleteById(14L);
-        Cliente cliente = new Cliente("119.298.263-03","Ana Zaira");
+        new ClienteDao().deleteById(20L);
+        new ClienteDao().deleteById(21L);
+        Cliente cliente = new Cliente("119.298.263-03", "Ana");
         System.out.println(cliente);
         long clienteId = new ClienteDao().saveOrUpdate(cliente);
         cliente.setId(clienteId);
+        cliente.setNome("Ana Zaira");
+        clienteId = new ClienteDao().saveOrUpdate(cliente);
         System.out.println(cliente);
-        Contrato contrato = new Contrato(clienteId,"Contrato por tempo determinado",
+
+        Contrato contrato = new Contrato(clienteId, "Contrato por tempo determinado",
                 LocalDate.parse("2023-05-21"));
         long contratoId = new ContratoDao().saveOrUpdate(contrato);
         contrato.setId(contratoId);
         System.out.println(contrato);
+
+        contrato = new Contrato(clienteId, "Contrato por tempo indeterminado",
+                LocalDate.parse("2023-12-01"));
+        contratoId = new ContratoDao().saveOrUpdate(contrato);
+        contrato.setId(contratoId);
+        System.out.println(contrato);
+        contrato.setUltimaAtualizacao(LocalDate.parse("2023-05-01"));
+        contratoId = new ContratoDao().saveOrUpdate(contrato);
+
+        contrato = new Contrato(clienteId, "Contrato de trabalho eventual2023",
+                LocalDate.parse("2023-05-26"));
+        contratoId = new ContratoDao().saveOrUpdate(contrato);
+        contrato.setId(contratoId);
+        System.out.println(contrato);
+        contrato.setRedacao("Contrato de trabalho eventual");
+        contratoId = new ContratoDao().saveOrUpdate(contrato);
+
+//////////////////////////////////////////
+        cliente = new Cliente("267.529.650-30", "267.529.650-30, Beatriz Yana");
+        System.out.println(cliente);
+        clienteId = new ClienteDao().saveOrUpdate(cliente);
+        cliente.setId(clienteId);
+        cliente.setNome("Beatriz Yana");
+        clienteId = new ClienteDao().saveOrUpdate(cliente);
+        System.out.println(cliente);
+
+        contrato = new Contrato(clienteId, "Contrato de estágio",
+                LocalDate.parse("2023-10-15"));
+        contratoId = new ContratoDao().saveOrUpdate(contrato);
+        contrato.setId(contratoId);
+        System.out.println(contrato);
+
+        contrato = new Contrato(clienteId, "Contrato de experiência",
+                LocalDate.parse("2023-05-01"));
+        contratoId = new ContratoDao().saveOrUpdate(contrato);
+        contrato.setId(contratoId);
+        System.out.println(contrato);
+        contrato.setUltimaAtualizacao(LocalDate.parse("2023-09-16"));
+        contratoId = new ContratoDao().saveOrUpdate(contrato);
+
+        contrato = new Contrato(clienteId, "Contrato de teletrabalho, 17/08/2023",
+                LocalDate.parse("2023-08-17"));
+        contratoId = new ContratoDao().saveOrUpdate(contrato);
+        contrato.setId(contratoId);
+        System.out.println(contrato);
+        contrato.setRedacao("Contrato de teletrabalho");
+        contratoId = new ContratoDao().saveOrUpdate(contrato);
+
+        contrato = new Contrato(clienteId, "Contrato intermitente",
+                LocalDate.parse("2023-07-15"));
+        contratoId = new ContratoDao().saveOrUpdate(contrato);
+        contrato.setId(contratoId);
+        System.out.println(contrato);
+
     }
 }
